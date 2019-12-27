d1ae8407204a1959eb4364ae1fc876e1f7735f71
Di Ying <yingdi1111@gmail.com>
2019-12-26 17:57:25 -0700

    initial load

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,23 @@ export class LoginComponent implements OnInit {
   constructor(public afAuth: AngularFireAuth , private router: Router) {
   }
   login() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    let provider = new auth.GoogleAuthProvider();
+    auth().signInWithRedirect(provider).then(function() {
+      return auth().getRedirectResult();
+    }).then(function(result) {
+      // This gives you a Google Access Token.
+      // You can use it to access the Google API.
+      var token = (result.credential as any).accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(result)
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(error)
+    });
   }
   logout() {
     this.afAuth.auth.signOut();
